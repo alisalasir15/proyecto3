b0acc85875ebd99155b79dbea720975b0fff8a67
Alisalasir15 <alisalasir15@gmail.com>
2021-03-30 18:30:18 +0200

    Primer proyecto CSharp

---
+++ CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretString =   "TmF2ZWdhIHBv" + "ciAgaHR0cHM6L" +
+                                    "y93d3cueW91d" + "HViZS5jb20vam" +
+                                    "F2aXRlcmFuIH" + "kgZGVzY3VicmU" +
+                                    "gbWlzIHbDrWR" + "lb3Mu";
+
+            Console.WriteLine(secretString);
+            byte[] theBytes = Convert.FromBase64String(secretString);
+            char[] theChars = Encoding.ASCII.GetChars(theBytes);
+            Console.WriteLine(new String(theChars));
+            Console.ReadLine();
+        }
+    }
+}
