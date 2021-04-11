58f55d4bbf99ba51af15b008f68c355c0db7c412
Javier Terán González <javiteran@hotmail.com>
2021-04-11 22:44:25 +0200

    Docker/docker-compose Inicio

---
+++ Temp/docker/wordpress/wp.sh
@@ -0,0 +1,6 @@
+docker run --name wordpress0 -p 8080:80 -d wordpress
+docker run --name wordpress1 -p 8081:80 -d wordpress
+docker run --name wordpress2 -p 8082:80 -d wordpress
+docker run --name wordpress3 -p 8083:80 -d wordpress
+docker run --name wordpress4 -p 8084:80 -d wordpress
+docker run --name wordpress5 -p 8085:80 -d wordpress
