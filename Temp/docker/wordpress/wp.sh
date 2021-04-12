44d114e0bd28fc356e6703c7efd3ba55cd248b6e
Javier Terán González <javiteran@hotmail.com>
2021-04-12 10:02:17 +0200

    wp.sh

--- Temp/docker/wordpress/wp.sh
+++ Temp/docker/wordpress/wp.sh
@@ -1,6 +1,7 @@
-docker run --name wordpress0 -p 8080:80 -d wordpress
-docker run --name wordpress1 -p 8081:80 -d wordpress
-docker run --name wordpress2 -p 8082:80 -d wordpress
-docker run --name wordpress3 -p 8083:80 -d wordpress
-docker run --name wordpress4 -p 8084:80 -d wordpress
-docker run --name wordpress5 -p 8085:80 -d wordpress
+#Creacion de N instancias docker de wordpress con su puerto de publicacion
+#!/bin/bash
+for i in {1..5}
+do
+  echo "docker run --name wordpress$i -p 808$i:80 -d wordpress"
+  docker run --name wordpress$i -p 808$i:80 -d wordpress
+done
