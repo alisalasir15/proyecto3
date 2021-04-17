b634703d01f518346fdad4a239caab45c3e350db
Alisalasir15 <alisalasir15@gmail.com>
2021-04-17 12:31:59 +0000

    Desde ubuntu 1

--- Powershell/01LeerCSV.ps1
+++ Powershell/01LeerCSV.ps1
@@ -1,8 +1,10 @@
 # Código para leer un archivo .csv y mostrar los datos de determinadas columnas
 # Javier Terán
 # 28/03/2021 
-
+#Esto funciona en ruta windows
 $ArchivoReunion  = $env:USERPROFILE +"\documents\GitHub\Proyecto3\Powershell\01LeerCSV.csv"
+#Esto funciona en ruta linux
+#$ArchivoReunion  = "/home/javiteran/GitHub/proyecto3-1/Powershell/01LeerCSV.csv"
 #Utilizando variables intermedias
 $misDatosReunion = Import-Csv $ArchivoReunion -Delimiter ";"
 foreach ($asistente in $misDatosReunion) {
