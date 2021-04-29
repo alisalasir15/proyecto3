8a47c614d7716b13f43a873e956207a207e9b334
Javier Terán González <javiteran@hotmail.com>
2021-04-29 18:10:46 +0200

    docker-docker-compose-dockerfile

--- Temp/docker/wordpress/wp.ps1
+++ Temp/docker/wordpress/wp.ps1
@@ -5,3 +5,11 @@ for ($i = 0; $i -lt 3; $i++) {
     docker run --name wordpress$i -p $puertos -d wordpress
 }
 
+
+#docker run -i -t ubuntu:20.04   /bin/bash
+#docker run --name wp8apache91 -p 8091:80 -d wordpress:php8.0-apache
+#docker run --name wp8apache92 -p 8092:80 -d wordpress:php8.0-apache
+#docker run --name mysql7 -e MYSQL_ROOT_PASSWORD=pass -d mysql:5.7
+#docker run --name mysql8 -e MYSQL_ROOT_PASSWORD=pass -d mysql:8
+#docker ps
+
--- Temp/docker/wordpress/wp.sh
+++ Temp/docker/wordpress/wp.sh
@@ -5,3 +5,11 @@ do
   echo "docker run --name wordpress$i -p 808$i:80 -d wordpress"
   docker run --name wordpress$i -p 808$i:80 -d wordpress
 done
+
+
+#docker run -i -t ubuntu:20.04   /bin/bash
+#docker run --name wp8apache91 -p 8091:80 -d wordpress:php8.0-apache
+#docker run --name wp8apache92 -p 8092:80 -d wordpress:php8.0-apache
+#docker run --name mysql7 -e MYSQL_ROOT_PASSWORD=pass -d mysql:5.7
+#docker run --name mysql8 -e MYSQL_ROOT_PASSWORD=pass -d mysql:8
+#docker ps
