63c0ccfe196d35fdb090d9bf904669192e6f152f
alisalasir15 <alisalasir15@gmail.com>
2021-04-30 14:36:06 +0000

    Despues del video 15

--- Contenedores/Docker/wp.ps1
+++ Contenedores/Docker/wp.ps1
@@ -4,4 +4,13 @@ docker run --name wp8apache93 -p 8093:80 -d wordpress:php8.0-apache
 docker run --name wp8apache94 -p 8094:80 -d wordpress:php7.3-apache
 docker run --name wp8apache95 -p 8095:80 -d wordpress:latest
 
-docker run --name pythonjavi -d python:alpine3.12
+#Ejecuta un contendor con python
+docker run --name pythonjavi1 -d python:alpine3.12
+
+#Ejecuta varios contenedores iguales con un bucle
+for ($i = 0; $i -lt 3; $i++) {
+    $puertos = (8100 + $i).ToString() + ':80'
+    #write-host ($puertos)
+    write-host("docker run --name wordpress$i -p $puertos -d wordpress")
+    docker run --name wordpress$i -p $puertos -d wordpress
+}
--- Contenedores/DockerfileUbApache/Dockerfile.ps1
+++ Contenedores/DockerfileUbApache/Dockerfile.ps1
@@ -9,4 +9,8 @@ docker run -ti  -d -p 8501:80 proyecto03ubapache21 /bin/bash
 #docker exec -ti  .....  /bin/bash
 
 #Crear un docker con almacenamiento compartido en el host que es servidor de docker
-docker run -v C:\Users\javiteran\Documents\GitHub\proyecto3\Contenedores\DockerfileUbApache\html:/var/www/html  -t -i -p 8501:80 proyecto03ubapache21 /bin/bash
+#Ruta Windows
+#docker run -v C:/Users/javiteran/Documents/GitHub/proyecto3/Contenedores/DockerfileUbApache/html:/var/www/html  -t -i -p 8501:80 proyecto03ubapache21 /bin/bash
+
+#Ruta Linux
+docker run -v /home/javiteran/GitHub/proyecto3/Contenedores/DockerfileUbApache/html:/var/www/html -d -it  -p 8501:80 proyecto03ubapache21 /bin/bash
