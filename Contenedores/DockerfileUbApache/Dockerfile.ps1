b43eaf4f8f97827da7eb4cc1407e83d1a1c76005
Alisalasir15 <alisalasir15@gmail.com>
2021-04-30 11:43:44 +0200

    docker con almacenamento compartido

---
+++ Contenedores/DockerfileUbApache/Dockerfile.ps1
@@ -0,0 +1,12 @@
+
+#Crea una imagen
+docker build . -t proyecto03ubapache21
+docker images
+
+#Ejecuta imagen
+docker run -ti  -d -p 8501:80 proyecto03ubapache21 /bin/bash
+
+#docker exec -ti  .....  /bin/bash
+
+#Crear un docker con almacenamiento compartido en el host que es servidor de docker
+docker run -v C:\Users\javiteran\Documents\GitHub\proyecto3\Contenedores\DockerfileUbApache\html:/var/www/html  -t -i -p 8501:80 proyecto03ubapache21 /bin/bash
