60ab5cb09523f9329930c5a069da4c56503ea45a
alisalasir15 <alisalasir15@gmail.com>
2021-04-30 15:03:13 +0000

    Instalacion CSharp en Ubuntu

--- CSharp/LeerCSV.cs
+++ CSharp/LeerCSV.cs
@@ -2,7 +2,11 @@
  *
  * @author Javier Terán González 02/04/2021
  * Lectura de un archivo CSV
-
+ 
+ * Instalar C# en linux.
+ *  sudo snap dotnet-sdk --classic
+ * https://docs.microsoft.com/en-us/dotnet/core/install/linux-snap#:~:text=Snap%20packages%20for%20the.NET%20SDK%20are%20all%20published,corresponding%20runtime.%20The%20following%20table%20lists%20the%20channels%3A
+ 
  * dotnet build
  * dotnet run
  */
